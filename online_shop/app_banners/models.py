67365e5de323b9f62ea1ad3fcb408b7854a7e1f6
Vladimir Chegodaev <chegodaev.vl@yandex.ru>
2022-04-01 22:21:49 +0300

    Added superuser Created Banners model

--- online_shop/app_banners/models.py
+++ online_shop/app_banners/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Banners(models.Model):
+    advertisementname = models.CharField(max_length=50, verbose_name='Рекламодатель')
+    bannerimg = models.ImageField(upload_to='img/', null=True, default=None, verbose_name='Рисунок баннера')
+    isactive = models.BooleanField(default=False, verbose_name='Активный')
+    bannerlink = models.TextField(verbose_name='Ссылка баннера')
+
+    class Meta:
+        verbose_name = 'Баннер'
+        verbose_name_plural = "Баннеры"
