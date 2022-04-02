2894a2a7a34603e771016b873552497d09823112
Vladimir Chegodaev <chegodaev.vl@yandex.ru>
2022-04-02 13:59:21 +0300

    Added models: Categories, Subcategories, Characteristictypes, Characteristics, Statistics

--- online_shop/app_categories/models.py
+++ online_shop/app_categories/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+        categoryname = models.CharField(max_length=50, verbose_name='Название категории')
+
+        class Meta:
+                verbose_name = 'Категория'
+                verbose_name_plural = 'Категории'
+
+        def __str__(self):
+                return self.categoryname
+
+
+class Subcategories(models.Model):
+        categoryidx = models.ForeignKey('Categories', on_delete=models.CASCADE, verbose_name='Родительская категория')
+        subcategoryname = models.CharField(max_length=50, verbose_name='Название подкатегории')
+        goodcharacteristics = models.TextField(verbose_name='Перечень характеристик')
+
+        class Meta:
+                verbose_name = 'Подкатегория'
+                verbose_name_plural = 'Подкатегории'
+
+        def __str__(self):
+                return self.subcategoryname
+
+
+
+class Characteristictypes(models.Model):
+        characteristictype = models.CharField(max_length=25, verbose_name='Тип характеристики')
+
+        class Meta:
+                verbose_name = 'Тип характеристик'
+                verbose_name_plural = 'Типы характеристик'
+
+
+class Characterisitcs(models.Model):
+        characterisitctype = models.ForeignKey('Characteristictypes', on_delete=models.CASCADE, verbose_name='Тип характеристики')
+        goodidx = models.ForeignKey('app_goods.Goods', on_delete=models.CASCADE, verbose_name='Товар')
+        value = models.CharField(max_length=5, verbose_name='Значение характеристики')
+
+        class Meta:
+                verbose_name = 'Характеристика'
+                verbose_name_plural = 'Характеристики'
--- online_shop/app_statistics/models.py
+++ online_shop/app_statistics/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Statisitcs(models.Model):
+    goodidx = models.ForeignKey('app_goods.Goods', on_delete=models.CASCADE, verbose_name='Товар')
+    quantity = models.IntegerField(verbose_name='Количество')
+    dt = models.ForeignKey('app_orders.Orders', on_delete=models.CASCADE, verbose_name='Дата продажи')
