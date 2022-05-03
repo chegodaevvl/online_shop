cef7617f516a121ee1cc2282399effa028e9a4c5
Vladimir Chegodaev <chegodaev.vl@yandex.ru>
2022-05-03 23:36:09 +0300

    1. Implemented Subcategory view (Subcategories related with Category) 2. Implemented Goods view (Goods related with Subcategory) 3. Implemented Goods detail view

--- online_shop/app_categories/urls.py
+++ online_shop/app_categories/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import CategoriesList, FeaturedCategoriesListView
+from .views import CategoriesList, FeaturedCategoriesListView, SubcategoriesView, GoodsList
 
 
 urlpatterns = [
     path('', CategoriesList.as_view(), name='categories'),
-    path('featured_categories', FeaturedCategoriesListView.as_view(), name='featured_categories')
+    path('featured_categories', FeaturedCategoriesListView.as_view(), name='featured_categories'),
+    path('cat-<int:cat_id>', SubcategoriesView.as_view(), name='subcategories'),
+    path('sub-<int:sub_id>', GoodsList.as_view(), name='goods'),
 ]
--- online_shop/app_categories/views.py
+++ online_shop/app_categories/views.py
@@ -1,6 +1,9 @@
-from django.views.generic import ListView
-from .models import Categories
+from django.views.generic import ListView, DetailView
+from django.views import View
+from django.shortcuts import render
+from .models import Categories, Subcategories
 from .utils import get_featured_categories
+from app_goods.models import Goods
 
 
 class CategoriesList(ListView):
@@ -11,3 +14,17 @@ class FeaturedCategoriesListView(ListView):
     queryset = get_featured_categories(quantity=3)
     context_object_name = 'featured_categories'
     template_name = 'app_categories/featured_categories.html'
+
+
+class SubcategoriesView(View):
+
+    def get(self, request, cat_id):
+        linked_subcategories = Subcategories.objects.filter(categoryidx=cat_id)
+        return render(request, 'app_categories/subcategories_list.html', context={'subcategories': linked_subcategories})
+
+
+class GoodsList(View):
+
+    def get(self, request, sub_id):
+        goods = Goods.objects.filter(categoryidx=sub_id)
+        return render(request, 'app_goods/goods_list.html', context={'goods': goods})
